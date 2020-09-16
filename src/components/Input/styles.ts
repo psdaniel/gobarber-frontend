412581d3d2ea94398ea1add51a9909d525f83ad9
Daniel Souza <daniel.n10@hotmail.com>
2020-09-16 10:27:59 -0300

    tooltip de erros

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
     isFocused: boolean;
     isFilled: boolean;
+    isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,6 +23,10 @@ export const Container = styled.div<ContainerProps>`
     & + div {
         margin-top: 8px;
     }
+
+    ${props => props.isErrored && css`
+        border-color: #c53030;
+    `}
     
     ${props => props.isFocused && css`
         color: #ff9000;
@@ -29,6 +36,7 @@ export const Container = styled.div<ContainerProps>`
     ${props => props.isFilled && css`
         color: #ff9000;
     `}
+
 
     input {
         flex: 1;
@@ -43,5 +51,23 @@ export const Container = styled.div<ContainerProps>`
 
     svg {
         margin-right: 16px;
+    }
+`;
+
+export const Error = styled(Tooltip)`
+    height: 20px;
+    margin-left: 16px;
+
+    svg {
+        margin: 0;
+    }
+
+    span {
+        background: #c53030;
+        color: #fff;
+
+        &&::before {
+            border-color: #c53030 transparent;
+        }
     }
 `;
