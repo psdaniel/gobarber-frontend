6e0d42243742cdeb6fc9a516237ac0ff81e35f69
Daniel Souza <daniel.n10@hotmail.com>
2020-09-18 00:39:06 -0300

    criação do hook de toast

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,8 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -23,9 +24,9 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const { user, signIn } = useAuth();
+    const { signIn } = useAuth();
+    const { addToast } = useToast();
 
-    console.log(user);
 
     const handleSubmit = useCallback(async (data: SignInFormData) => {
         try {
@@ -42,7 +43,7 @@ const SignIn: React.FC = () => {
             abortEarly: false, 
         });
 
-        signIn({
+        await signIn({
             email: data.email,
             password: data.password,
         });
@@ -53,8 +54,9 @@ const SignIn: React.FC = () => {
             formRef.current?.setErrors(errors);
         }
 
+        addToast();
     }
-}, [signIn]);
+}, [signIn, addToast]);
 
     return (
     <Container>
