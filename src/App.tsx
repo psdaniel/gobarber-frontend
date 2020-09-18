07b4920c3a9919ffdc10859df44df70e069687fe
Daniel Souza <daniel.n10@hotmail.com>
2020-09-17 23:44:28 -0300

    estrutura das mensagens de toast

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 //import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -11,6 +12,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -47,11 +47,12 @@ const SignIn: React.FC = () => {
             password: data.password,
         });
     } catch (err) {
-        console.log(err);
+        if (err instanceof Yup.ValidationError) {
+            const errors = getValidationErrors(err);
 
-        const errors = getValidationErrors(err);
+            formRef.current?.setErrors(errors);
+        }
 
-        formRef.current?.setErrors(errors);
     }
 }, [signIn]);
 
