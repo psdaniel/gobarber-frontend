00bfbcbc8145f1caa530583bf46190fb461505ac
Daniel Souza <daniel.n10@hotmail.com>
2020-09-30 15:59:17 -0300

    listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
+import { 
+    Container, 
+    Header, 
+    HeaderContent, 
+    Profile, 
+    Content, 
+    Schedule, 
+    NextAppointment, 
+    Calendar,
+    Section,
+    Appointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { FiClock, FiPower } from 'react-icons/fi';
@@ -8,6 +19,8 @@ import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
     const { signOut, user } = useAuth();
 
     return (
@@ -52,6 +65,30 @@ const Dashboard: React.FC = () => {
                         </span>
                     </div>
                 </NextAppointment>
+
+                <Section>
+                    <strong>Manhã</strong>
+
+                    <Appointment>
+                        <span>
+                            <FiClock />
+                            08:00
+                        </span>
+
+                        <div>
+                            <img
+                                src="ddd"
+                                alt="Daniel Souza"
+                                />
+
+                                <strong>Daniel Souza</strong>
+                        </div>
+                    </Appointment>
+                </Section>
+
+                <Section>
+                    <strong>Tarde</strong>
+                </Section>
             </Schedule>
             <Calendar />
         </Content>
