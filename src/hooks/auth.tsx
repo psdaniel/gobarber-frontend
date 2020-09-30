446c492382d0f233219d40bbb44eef98751a641f
Daniel Souza <daniel.n10@hotmail.com>
2020-09-30 19:50:20 -0300

    exibição da disponibilidade do mês

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,8 +31,11 @@ const AuthProvider: React.FC = ({ children }) => {
         const user = localStorage.getItem('@GoBarber:user');
 
         if(token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
             return { token, user: JSON.parse(user) };
         }
+
 
         return {} as AuthState;
     });
@@ -47,6 +50,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
         localStorage.setItem('@GoBarber:token', token);
         localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+        api.defaults.headers.authorization = `Bearer ${token}`;
 
         setData({ token, user });
     }, [])
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
@@ -19,9 +19,20 @@ import logoImg from '../../assets/logo.svg';
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+interface MonthAvailabilityItem {
+    day: number;
+    available: boolean;
+}
 
 const Dashboard: React.FC = () => {
+    const { user, signOut } = useAuth();
+
     const [selectedDate, setSelectedDate] = useState(new Date());
+    const [currentMonth, setCurrentMonth] = useState(new Date());
+
+    const [monthAvailability, setMonthAvailability] = useState<MonthAvailabilityItem[]>([]);
 
     const handleDateChange = useCallback((day: Date, modifiers: DayModifiers ) => {
         if (modifiers.available) {
@@ -29,7 +40,32 @@ const Dashboard: React.FC = () => {
         }
     }, [])
 
-    const { signOut, user } = useAuth();
+    const handleMonthChange = useCallback((month: Date) => {
+        setCurrentMonth(month);
+    }, []);
+
+    useEffect(() => {
+        api.get(`/providers/${user.id}/month-availability`, {
+            params: {
+                year: currentMonth.getFullYear(),
+                month: currentMonth.getMonth() + 1,
+            }
+        }).then(response => {
+            setMonthAvailability(response.data);
+        })
+    }, [currentMonth, user.id]);
+
+    const disabledDays = useMemo(() => {
+        const dates = monthAvailability.filter(monthDay => monthDay.available === false)
+        .map(monthDay => {
+            const year = currentMonth.getFullYear();
+            const month = currentMonth.getMonth();
+
+            return new Date(year, month, monthDay.day);
+        })
+
+        return dates;
+    }, [currentMonth, monthAvailability])
 
     return (
         <Container>
@@ -103,12 +139,12 @@ const Dashboard: React.FC = () => {
                     weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
                     fromMonth={new Date()}
                     disabledDays={[
-                        { daysOfWeek: [0, 6]}
-                    ]}
+                        { daysOfWeek: [0, 6]}, ...disabledDays]}
                     modifiers={{
                         available: { daysOfWeek: [1, 2, 3, 4, 5]}
                     }}
                     selectedDays={selectedDate}
+                    onMonthChange={handleMonthChange}
                     onDayClick={handleDateChange}
                     months={[
                         'Janeiro',
